a1387287b8e40e204f86a9901eaa1886f1aea59d
Meiliana Zalianti <104289561+MeilianaZ@users.noreply.github.com>
2022-05-21 14:55:36 +0700

    Create Latihan8.py

---
+++ MeilianaZ/Pertemuan-2/Latihan8.py
@@ -0,0 +1,4 @@
+weight = float(input('masukkan berat badan anda dalam kg = '))
+height = float(input('masukkan tinggi badan anda dalam m = '))
+BMI = weight / (height * height)
+print('Indeks massa tubuh anda adalah',BMI)
