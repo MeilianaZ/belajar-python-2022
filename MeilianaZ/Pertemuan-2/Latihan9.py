7e7d41c63bb4f5730ecf0c9cf12821fe84e51504
Meiliana Zalianti <104289561+MeilianaZ@users.noreply.github.com>
2022-05-21 15:04:33 +0700

    Create Latihan9.py

---
+++ MeilianaZ/Pertemuan-2/Latihan9.py
@@ -0,0 +1,5 @@
+tekanan = float(input('masukkan tekanan anda dalam kilopascal = '))
+psi = tekanan*0.145
+atm = tekanan*0.00987
+mmhg = tekanan*7.50062
+print('tekanan anda',tekanan,'kilopascal','\ntekanan anda',psi,'psi','\ntekanan anda',atm,'atm','\ntekanan anda',mmhg,'mmhg')
