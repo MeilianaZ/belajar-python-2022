68a253a989260d3bd14233545767ce5d2f4d4b6b
Meiliana Zalianti <104289561+MeilianaZ@users.noreply.github.com>
2022-07-21 09:23:55 +0700

    Create Latihan9.py

---
+++ MeilianaZ/Pertemuan-2/Latihan9.py
@@ -0,0 +1,6 @@
+tekanan = float(input('masukkan tekanan anda dalam kilopascal = '))
+psi = tekanan*0.145
+atm = tekanan*0.00987
+mmhg = tekanan*7.50062
+print('tekanan anda',tekanan,'kilopascal','\ntekanan anda',psi,'psi','\ntekanan anda',atm,'atm','\ntekanan anda',mmhg,'mmhg')
+0 comments on commit 7e7d41c
