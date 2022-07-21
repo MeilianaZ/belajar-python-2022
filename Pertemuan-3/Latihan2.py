1a2a4c8728d8336164e4da32d1a75db3574ec049
Meiliana Zalianti <104289561+MeilianaZ@users.noreply.github.com>
2022-07-21 09:31:26 +0700

    Create Latihan2.py

---
+++ Pertemuan-3/Latihan2.py
@@ -0,0 +1,19 @@
+s=int(input('Sisi = '))
+if s==3:
+    print('Itu adalah bentuk segitiga')
+elif s==4:
+    print('Itu adalah bentuk segi empat')
+elif s==5:
+    print('Itu adalah bentuk segi lima')
+elif s==6:
+    print('Itu adalah bentuk segi enam')
+elif s==7:
+    print('Itu adalah bentuk segi tujuh')
+elif s==8:
+    print('Itu adalah bentuk segi delapan')
+elif s==9:
+    print('Itu adalah bentuk segi sembilan')
+elif s==10:
+    print('Itu adalah bentuk segi sepuluh')
+else:
+    print('Maaf, ilmu saya belum sampai situ')
